2cab2ee8fb45b33af4d613612a44aae4c72bab18
abib <abib.alimi@inria.fr>
2021-04-19 23:55:57 +0200

    Fix style issues, should get a 'Good Style' now :pray:

--- 00-Setup/02-Git/today.py
+++ 00-Setup/02-Git/today.py
@@ -2,10 +2,8 @@
 
 def my_name_is():
     """Returns my name"""
-    my_name = "olushO"
-    return my_name
+    return "olushO"
 
 def my_buddy_is():
     """Returns my buddy's name"""
-    my_buddy = "AlexandrePerdomo"
-    return my_buddy
+    return "AlexandrePerdomo"
