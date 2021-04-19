41ba7bbd30c4addedcae5d30ecec94a6df46eab8
abib <abib.alimi@inria.fr>
2021-04-19 23:46:45 +0200

    Fix style issues, should get a 'Good Style' now :pray:

--- 00-Setup/02-Git/today.py
+++ 00-Setup/02-Git/today.py
@@ -1,7 +1,11 @@
+"""A module computing buddy pair names for the day"""
+
 def my_name_is():
+    """Returns my name"""
     MY_NAME = "olushO"
     return MY_NAME
 
 def my_buddy_is():
+    """Returns my buddy's name"""
     MY_BUDDY = "AlexandrePerdomo"
     return MY_BUDDY
