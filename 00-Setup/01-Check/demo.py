c3862b363f5426d61be6f382c973a6a8c50121b5
abib <abib.alimi@inria.fr>
2021-04-19 23:15:45 +0200

    Solved the first challenge of the bootcamp :)

--- 00-Setup/01-Check/demo.py
+++ 00-Setup/01-Check/demo.py
@@ -1,6 +1,6 @@
 """Dummy challenge for Kitt Demo"""
-
+import numpy as np
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    pass
+    return np.pi * radius ** 2 if radius >= 0 else 0
