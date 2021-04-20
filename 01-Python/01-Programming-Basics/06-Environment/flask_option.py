bc0472855c14c15d7bf43c1df823075c3995b1f4
abib <abib.alimi@inria.fr>
2021-04-20 18:31:01 +0200

    Environment variables as alternative to command line arguments

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -5,7 +5,13 @@ import os
 
 def start():
     """returns the right message"""
-    pass
+    if 'FLASK_ENV' not in os.environ:
+        message = "Starting in production mode..."
+    elif os.environ['FLASK_ENV'] == 'development':
+        message = "Starting in development mode..."
+    elif os.environ['FLASK_ENV'] == 'production':
+        message = "Starting in production mode..."
+    return message
 
 
 if __name__ == "__main__":
