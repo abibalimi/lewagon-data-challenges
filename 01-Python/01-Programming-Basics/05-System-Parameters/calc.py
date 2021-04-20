504183d580c3dc934a159164a4982a65ec98f59c
abib <abib.alimi@inria.fr>
2021-04-20 16:59:15 +0200

    Dealing with system parameters

--- 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
@@ -1,10 +1,16 @@
-# pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
+# pylint: disable=missing-module-docstring, missing-function-docstring, eval-used
 import sys
-
 
 def main():
     """Implement the calculator"""
-    pass
+    if sys.argv[2] == "+":
+        result = int(sys.argv[1]) + int(sys.argv[3])
+    elif sys.argv[2] == "-":
+        result = int(sys.argv[1]) - int(sys.argv[3])
+    elif sys.argv[2] == "*":
+        result = int(sys.argv[1]) * int(sys.argv[3])
+    return result
+
 
 
 if __name__ == "__main__":
