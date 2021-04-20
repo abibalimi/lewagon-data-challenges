1b44a7fc2eff853c94e059884d4d1133ae6a6459
abib <abib.alimi@inria.fr>
2021-04-20 23:39:31 +0200

    sudoku validator test blocks at 'test_invalid_grid_column'

--- 01-Python/01-Programming-Basics/07-Optional-Sudoku/sudoku.py
+++ 01-Python/01-Programming-Basics/07-Optional-Sudoku/sudoku.py
@@ -1,4 +1,24 @@
 # pylint: disable=missing-docstring
 
 def sudoku_validator(grid):
-    pass
+    ok_line = True
+    ok_col = True
+    ok_block = True
+    while ok_line and ok_col and ok_block:
+        for i in range(9):
+            ok_line = sorted(grid[i]) == sorted([j+1 for j in range(9)])
+        for i in range(9):
+            col = []
+            for j in range(9):
+                col.append(grid[j][i])
+            ok_col = sorted(col) == sorted([j+1 for j in range(9)])
+            col[:] = []
+        for k in [0, 1, 2]:
+            for m_l in [0, 1, 2]:
+                block = []
+                for i in range(3):
+                    for j in range(3):
+                        block.append(grid[m_l*3+i][k*3+j])
+                ok_block = sorted(block) == sorted([j+1 for j in range(9)])
+                block[:] = []
+    return ok_line and ok_col and ok_block
