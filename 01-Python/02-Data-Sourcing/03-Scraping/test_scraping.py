33f478a3a502784e6ad7682dde71c5dc81a081b7
abib <abib.alimi@inria.fr>
2021-04-21 15:43:24 +0200

    Download search result and perform offline scraping

--- 01-Python/02-Data-Sourcing/03-Scraping/test_scraping.py
+++ 01-Python/02-Data-Sourcing/03-Scraping/test_scraping.py
@@ -1,3 +1,9 @@
-# pylint: disable=missing-docstring,invalid-name
+# pylint: disable=missing-docstring,invalid-name,fixme
 
 # TODO: paste the code from Kitt's instructions
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open("pages/carrot.html"), "html.parser")
+
+for recipe in soup.find_all('p', class_= 'recipe-name'):
+    print(recipe.text)
