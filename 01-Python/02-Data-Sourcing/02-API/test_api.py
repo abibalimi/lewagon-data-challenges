6f50c1705f80b7e5d9e33211768f262c0df1170f
abib <abib.alimi@inria.fr>
2021-04-21 12:46:40 +0200

    Testing the MetaWeather API

--- 01-Python/02-Data-Sourcing/02-API/test_api.py
+++ 01-Python/02-Data-Sourcing/02-API/test_api.py
@@ -1,3 +1,9 @@
-# pylint: disable=missing-docstring,invalid-name
+# pylint: disable=missing-docstring,invalid-name,fixme
 
 # TODO: paste the code from Kitt's instructions
+import requests
+
+url = "https://www.metaweather.com/api/location/search/?query=london"
+response = requests.get(url).json()
+city = response[0]
+print(f"{city['title']}: {city['woeid']} ({city['latt_long']})")
