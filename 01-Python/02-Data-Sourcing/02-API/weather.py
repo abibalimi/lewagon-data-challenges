be7329055fc754f2832379ecf572046754058d6a
abib <abib.alimi@inria.fr>
2021-04-21 15:18:18 +0200

    Commit of the Weather CLI

--- 01-Python/02-Data-Sourcing/02-API/weather.py
+++ 01-Python/02-Data-Sourcing/02-API/weather.py
@@ -1,34 +1,47 @@
 # pylint: disable=missing-docstring, fixme
 
 import sys
-import urllib.parse
 import requests
 
 BASE_URI = "https://www.metaweather.com"
+DEGREE_SIGN = u"\N{DEGREE SIGN}"
 
 
 def search_city(query):
-    # TODO: Look for a given city and disambiguate between several candidates. Return one city (or None)
+    # TODO: Look for a given city and disambiguate between several candidates.
+    # Return one city (or None)
     url = BASE_URI + "/api/location/search/?query=" + query
     response = requests.get(url).json()
     try:
         city = response[0]
-        print(f"{city})")
+        #print(f"{city})")
         #print(f"{city['title']}: {city['woeid']} ({city['latt_long']})")
+        print(f"Here is the weather in {city['title']}")
+        return city
     except IndexError:
         print(f"Sorry, MetaWeather does not know abour {query}...")
+        return None
 
 
 def weather_forecast(woeid):
+    #import pdb; pdb.set_trace()
     # TODO: Return a 5-element list of weather forecast for a given woeid
-    pass
+    url = f"{BASE_URI}/api/location/{woeid}"
+    response = requests.get(url).json()
+    return response['consolidated_weather']
 
 
 def main():
     query = input("City?\n> ")
-    city = search_city(query)
-    # TODO: Display weather forecast for a given city
-
+    if query:
+        city = search_city(query)
+        # TODO: Display weather forecast for a given city
+        if city:
+            woeid = city['woeid']
+            forecasts = weather_forecast(woeid)
+            for forecast in forecasts[0:5]:
+                print(f"{forecast['applicable_date']}: {forecast['weather_state_name']}"
+                      f" {round(forecast['max_temp'], 1)}{DEGREE_SIGN}C")
 
 if __name__ == '__main__':
     try:
