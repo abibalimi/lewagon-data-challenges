1ae06f68e851d440c10dfe6305c10f757e36fb0f
abib <abib.alimi@inria.fr>
2021-04-23 10:54:43 +0200

    Warming up with SELECT on school.sqlite db

--- 01-Python/04-SQL-Advanced/00-Warmup/school.py
+++ 01-Python/04-SQL-Advanced/00-Warmup/school.py
@@ -1,15 +1,25 @@
-# pylint:disable=C0111,C0103
+# pylint:disable=C0111,C0103,W0621
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
+db = conn.cursor()
+
+#import pdb; pdb.set_trace()
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass
-
+    city = (f'{city.upper()}',)
+    query="""
+        SELECT first_name, last_name
+        FROM students
+        WHERE UPPER(birth_city) = ?
+    """
+    db.execute(query, city)
+    results = db.fetchall() # results in a list (rows) of tuples (columns)
+    return results
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
 #   $ python school.py
 #
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+
+print(students_from_city(db, 'berlin'))
