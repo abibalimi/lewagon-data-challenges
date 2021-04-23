c2df35c19d70fadf7c22887d1f897d02564f7307
abib <abib.alimi@inria.fr>
2021-04-23 16:12:38 +0200

    Rank and partition: order and order cummulative amount per customer

--- 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
+++ 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
@@ -1,7 +1,35 @@
 # pylint:disable=C0111,C0103
 
 def order_rank_per_customer(db):
-    pass
+    query="""
+        SELECT
+            OrderID,
+            CustomerID,
+            OrderDate,
+            RANK() OVER (
+                PARTITION BY CustomerID
+                ORDER BY OrderDate
+            ) AS order_rank
+        FROM orders
+    """
+    db.execute(query)
+    results = db.fetchall() # results in a list (rows) of tuples (columns)
+    #results = [r[0] for r in results]
+    return results
 
 def order_cumulative_amount_per_customer(db):
-    pass
+    query="""
+        SELECT DISTINCT
+        o.OrderID,
+        o.CustomerID,
+        o.OrderDate,
+        SUM(od.UnitPrice * od.Quantity) OVER (
+            PARTITION BY o.CustomerID
+            ORDER BY o.OrderDate
+        ) AS OrderCumulativeAmount
+    FROM Orders o 
+    LEFT JOIN OrderDetails od ON od.OrderID = o.OrderID
+    """
+    db.execute(query)
+    results = db.fetchall()
+    return results
