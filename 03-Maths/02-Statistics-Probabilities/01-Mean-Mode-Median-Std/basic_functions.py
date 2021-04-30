c31a05f2e2ac141ce75c830feaffa4b92a910af6
abib <abib.alimi@inria.fr>
2021-04-30 12:35:12 +0200

    Completed basic functions

--- 03-Maths/02-Statistics-Probabilities/01-Mean-Mode-Median-Std/basic_functions.py
+++ 03-Maths/02-Statistics-Probabilities/01-Mean-Mode-Median-Std/basic_functions.py
@@ -1,14 +1,24 @@
+# pylint:disable=C0111,C0103
+from collections import Counter
+
 def my_mean(samples):
-    pass
+    return sum(samples)/len(samples)
 
 
 def my_standard_deviation(samples):
-    pass
+    mean_ = my_mean(samples)
+    squared_sum = sum([(x-mean_) ** 2 for x in samples])
+    return (squared_sum/(len(samples) -1)) ** 0.5
 
 
 def my_median(samples):
-    pass
+    samples.sort()
+    mid = len(samples) // 2
+    if len(samples) % 2 :
+        return samples[mid]
+    return (samples[mid-1]+samples[mid]) / 2
 
 
 def my_mode(samples):
-    pass
+    compteur = Counter(samples)
+    return [k for k, v in compteur.items() if v == compteur.most_common(1)[0][1]][0]
