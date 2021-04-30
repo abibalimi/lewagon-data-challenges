8a1e35bc38a558d81784d40a9ede4497882ccd40
abib <abib.alimi@inria.fr>
2021-04-30 14:51:13 +0200

    Four flips completed git add flip_coin_factorial.py

--- 03-Maths/02-Statistics-Probabilities/02-Toss-a-Coin/flip_coin_factorial.py
+++ 03-Maths/02-Statistics-Probabilities/02-Toss-a-Coin/flip_coin_factorial.py
@@ -4,15 +4,16 @@ import math
 
 
 def count_possibilities(n_toss, n_heads):
-    '''TO DO: '''
-    pass
+    return math.factorial(n_toss)/(math.factorial(n_heads)*math.factorial(n_toss - n_heads))
 
 
 def count_total_possibilities(n_toss):
-    '''TO DO: '''
-    pass
+    return 2 ** n_toss
 
 
 def probability(n_toss):
-    '''TO DO: '''
-    pass
+    proba = {}
+    for toss in range(n_toss+1):
+        print(f'{toss}: {count_possibilities(n_toss, toss)/count_total_possibilities(n_toss)}')
+        proba[toss] = count_possibilities(n_toss, toss)/count_total_possibilities(n_toss)
+    return proba
