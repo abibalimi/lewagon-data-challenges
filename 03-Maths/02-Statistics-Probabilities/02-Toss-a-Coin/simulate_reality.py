fa210201fac42b28040eb036ec06138ddb96135c
abib <abib.alimi@inria.fr>
2021-05-02 04:15:53 +0200

    Coin flipping Theory vs Reality: Completed

--- 03-Maths/02-Statistics-Probabilities/02-Toss-a-Coin/simulate_reality.py
+++ 03-Maths/02-Statistics-Probabilities/02-Toss-a-Coin/simulate_reality.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,W0612
 
 import random
 import numpy as np
@@ -19,5 +19,15 @@ def play_n_game(n_games, n_toss):
     The values will correspond to the probability of a game ending with that
      number of heads.
     '''
-    head_counter = [play_one_game(n_toss) for game in range(n_games+1)]
-    return dict(zip(np.arange(n_toss+1), np.bincount(head_counter, minlength=n_toss+1)/n_games))
+    #head_counter = [play_one_game(n_toss) for game in range(n_games+1)]
+    #return dict(zip(np.arange(n_toss+1), np.bincount(head_counter, minlength=n_toss+1)/n_games)) """
+    
+    # initialize dict with zeros
+    count_dict = {k: 0 for k in range(n_toss+1)}
+    for _ in range(n_games):
+        n_heads = play_one_game(n_toss)
+        count_dict[n_heads] += 1
+
+    # convert count to ratios
+    result = {k: v/n_games for k, v in count_dict.items()}
+    return result
