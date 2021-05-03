f9effa622b6f202e4fc310b03ceb8c58e258935d
abib <abib.alimi@inria.fr>
2021-05-03 15:28:31 +0200

    Completed Notebook and scrypt

--- 04-Decision-Science/olist/data.py
+++ 04-Decision-Science/olist/data.py
@@ -5,6 +5,7 @@ import pandas as pd
 class Olist:
 
     def get_data(self):
+        #import ipdb; ipdb.set_trace()
         """
         This function returns a Python dict.
         Its keys should be 'sellers', 'orders', 'order_items' etc...
@@ -15,7 +16,15 @@ class Olist:
         # Use __file__ as absolute path anchor independant of your computer
         # Make extensive use of `import ipdb; ipdb.set_trace()` to investigate what `__file__` variable is really
         # Use os.path library to construct path independent of Unix vs. Windows specificities
+        
+        csv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data','csv')
+        file_names = [fname for fname in os.listdir(csv_path) if fname.endswith('.csv')]
+        key_names = [kname[6:-12] if '_dataset.csv' in kname else kname[:-4] for kname in file_names]
+        data_frames = [pd.read_csv(os.path.join(csv_path, csv)) for csv in file_names]
+        data = {key: value for (key,value) in zip(key_names,data_frames)}
 
+        return data
+        
     def get_matching_table(self):
         """
         01-01 > This function returns a matching table between
@@ -27,3 +36,6 @@ class Olist:
         You call ping I print pong.
         """
         print('pong')
+
+ol = Olist()
+ol.get_data()
