1a2006f0cb8e0beb70c2a3009b889d5044952262
abib <abib.alimi@inria.fr>
2021-05-05 11:44:48 +0200

    order.py partially corrected

--- 04-Decision-Science/olist/order.py
+++ 04-Decision-Science/olist/order.py
@@ -88,21 +88,21 @@ class Order:
         items = self.data['order_items'].copy()
         return items[['order_id','price','freight_value']].groupby('order_id').sum()
 
-    def get_distance_seller_customer(self):
-        """
-        02-01 > Returns a DataFrame with order_id
-        and distance between seller and customer
-        """
+    #def get_distance_seller_customer(self):
+    #    """
+    #    02-01 > Returns a DataFrame with order_id
+    #    and distance between seller and customer
+    #    """
         # Optional
         # Hint: you can use the haversine_distance logic coded in olist/utils.py
 
-    def get_training_data(self, is_delivered=True,
+        """     def get_training_data(self, is_delivered=True,
                           with_distance_seller_customer=False):
         """
-        02-01 > Returns a clean DataFrame (without NaN), with the following columns:
-        [order_id, wait_time, expected_wait_time, delay_vs_expected, order_status,
-        dim_is_five_star, dim_is_one_star, review_score, number_of_products,
-        number_of_sellers, freight_value, distance_customer_seller]
+        #02-01 > Returns a clean DataFrame (without NaN), with the following columns:
+        #[order_id, wait_time, expected_wait_time, delay_vs_expected, order_status,
+        #dim_is_five_star, dim_is_one_star, review_score, number_of_products,
+        #number_of_sellers, freight_value, distance_customer_seller]
         """
         # Hint: make sure to re-use your instance methods defined above
         
@@ -112,6 +112,100 @@ class Order:
                     .merge(self.get_number_sellers(), on='order_id')\
                         .merge(self.get_price_and_freight(), on='order_id')
         
-        return result.dropna()
+        return result.dropna() """
                 
-        
+        # Optional
+    def get_distance_seller_customer(self):
+        """
+        02-01 > Returns a DataFrame with order_id
+        and distance between seller and customer
+        """
+
+        # import data
+
+        data = self.data
+        matching_table = Olist().get_matching_table()
+
+        # Since one zipcode can map to multiple (lat, lng), take first one
+        geo = data['geolocation']
+        geo = geo.groupby('geolocation_zip_code_prefix',
+                          as_index=False).first()
+
+        # Select sellers and customers
+        sellers = data['sellers']
+        customers = data['customers']
+
+        # Merge geo_location for sellers
+        sellers_mask_columns = ['seller_id', 'seller_zip_code_prefix',
+                                'seller_city', 'seller_state',
+                                'geolocation_lat', 'geolocation_lng']
+
+        sellers_geo = sellers.merge(
+            geo,
+            how='left',
+            left_on='seller_zip_code_prefix',
+            right_on='geolocation_zip_code_prefix')[sellers_mask_columns]
+
+        # Merge geo_location for customers
+        customers_mask_columns = ['customer_id', 'customer_zip_code_prefix',
+                                  'customer_city', 'customer_state',
+                                  'geolocation_lat', 'geolocation_lng']
+
+        customers_geo = customers.merge(
+            geo,
+            how='left',
+            left_on='customer_zip_code_prefix',
+            right_on='geolocation_zip_code_prefix')[customers_mask_columns]
+
+        # Use the matching table and merge customers and sellers
+        matching_geo = matching_table.merge(sellers_geo,
+                                            on='seller_id')\
+                                     .merge(customers_geo,
+                                            on='customer_id',
+                                            suffixes=('_seller',
+                                                      '_customer'))
+        # Remove na()
+        matching_geo = matching_geo.dropna()
+
+        matching_geo.loc[:, 'distance_seller_customer'] =\
+            matching_geo.apply(
+                lambda row: haversine_distance(
+                    row['geolocation_lng_seller'],
+                    row['geolocation_lat_seller'],
+                    row['geolocation_lng_customer'],
+                    row['geolocation_lat_customer']), axis=1)
+        # Since an order can have multiple sellers,
+        # return the average of the distance per order
+        order_distance =\
+            matching_geo.groupby(
+                'order_id',
+                as_index=False).agg({'distance_seller_customer': 'mean'})
+
+        return order_distance
+
+    def get_training_data(self, is_delivered=True,
+                          with_distance_seller_customer=False):
+        """
+        02-01 > Returns a clean DataFrame (without NaN), with the following
+        columns: [order_id, wait_time, expected_wait_time, delay_vs_expected,
+        dim_is_five_star, dim_is_one_star, review_score, number_of_products,
+        number_of_sellers, freight_value, distance_customer_seller]
+        """
+        # Hint: make sure to re-use your instance methods defined above
+        training_set =\
+            self.get_wait_time(is_delivered)\
+                .merge(
+                self.get_review_score(), on='order_id'
+               ).merge(
+                self.get_number_products(), on='order_id'
+               ).merge(
+                self.get_number_sellers(), on='order_id'
+               ).merge(
+                self.get_price_and_freight(), on='order_id'
+               )
+        # Skip heavy computation of distance_seller_customer unless specified
+        if with_distance_seller_customer:
+            training_set = training_set.merge(
+                self.get_distance_seller_customer(), on='order_id')
+
+        return training_set.dropna()
